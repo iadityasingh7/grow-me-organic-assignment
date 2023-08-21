1497e79145d8241bb9c1d594e14f112211d76ef4
Aditya Pratap Singh <isinghaditya7@gmail.com>
2023-08-21 22:52:26 +0530

    Routing fixes and access to second page prevented

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,6 @@ import Next from "./Pages/Next/Next";
 import Header from "./Components/Header/Header";
 
 function App() {
-  let userData: any = null;
-  const userDataJSON = localStorage.getItem("user_data");
-  if (userDataJSON && userDataJSON.length > 1) {
-    userData = JSON.parse(userDataJSON);
-  }
-
-  
-  function validateFormData(userData: any) {
-    return (userData?.Name &&
-    userData.Name.length > 0 &&
-    userData?.Phone &&
-    userData.Phone.length > 0 &&
-    userData?.Email &&
-    userData.Email.length > 0)
-  }
 
   return (
     <BrowserRouter>
@@ -28,7 +13,7 @@ function App() {
         <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
-          {validateFormData(userData) && <Route path="/next" element={<Next />} />}
+          <Route path="/next" element={<Next />} />
           <Route path="*" element={<Home />} />
         </Routes>
       </div>
--- src/Pages/Next/Next.tsx
+++ src/Pages/Next/Next.tsx
@@ -5,20 +5,43 @@ import ApiDataTable from "../../Components/ApiDataTable/ApiDataTable";
 
 function Next() {
   let userData: any = null;
-  console.log(userData);
   const userDataJSON = localStorage.getItem("user_data");
 
   if (userDataJSON && userDataJSON.length > 1) {
     userData = JSON.parse(userDataJSON);
   }
 
-  return (
-    <section>
-      <h1 className="next-h1">Second Page</h1>
-      <ApiDataTable />
-      <DepartmentsCategory />
-    </section>
-  );
+  function validateFormData(userData: any) {
+    return (
+      userData?.Name &&
+      userData.Name.length > 0 &&
+      userData?.Phone &&
+      userData.Phone.length > 0 &&
+      userData?.Email &&
+      userData.Email.length > 0
+    );
+  }
+
+  let pageUI;
+  if (validateFormData(userData)) {
+    pageUI = (
+      <section>
+        <h1 className="next-h1">Second Page</h1>
+        <ApiDataTable />
+        <DepartmentsCategory />
+      </section>
+    );
+  } else {
+    pageUI = (
+      <div>
+        Don't have access to second page. Go to home page and fill the form
+        first.
+        <a href="http://localhost:5173/">Home Page</a>
+      </div>
+    );
+  }
+
+  return <>{pageUI}</>;
 }
 
 export default Next;
